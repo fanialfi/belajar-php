b5041a3bf3b4334417ca86a872e69c836e159a96
fanialfi <fanialfirdaus@gmail.com>
2024-12-03 14:26:18 +0700

    modify

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-  echo "Hello World";
+  $name = "fani alfirdaus";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belajar PHP guys</title>
+</head>
+<body>
+  <h1>hai nama saya <?php echo $name ?></h1>
+</body>
+</html>
